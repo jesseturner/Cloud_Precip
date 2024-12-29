760a0f9a24e953497bc7c0d1a240dc362d2910e6
Jesse Turner <jesseturner72@gmail.com>
2024-12-29 13:41:43 -0700

    Plotting cocorahs precip amounts

---
+++ download_cocorahs.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+for date in "12/25/2024";
+
+do
+
+    clean_date="${date//\//}"
+    url="https://data.cocorahs.org/export/exportreports.aspx?ReportType=Daily&dtf=1&Format=XML&ReportDateType=reportdate&Date="$date"&TimesInGMT=True"
+    echo $url
+
+	wget -O cocorahs/$clean_date $url
+    echo "Cocorahs collected for "$date
+
+done
